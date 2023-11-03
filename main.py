1654ffb140942da1e66c9463c387162ed2eaac9b
René Delion <rene.leon.delion@gmail.com>
2023-11-03 17:00:59 +0100

    Fix typos

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ from question_model import Question
 
 question_bank = []
 for q in question_data:
-    question_bank.append(Question(q_text=q["text"], q_answer=q["answer"]))
-
-print(question_bank[0].text)
+    question_bank.append(Question(q["text"], q["answer"]))
--- question_model.py
+++ question_model.py
@@ -1,4 +1,4 @@
 class Question:
-    def __init__(self, q_text, q_anwser) -> None:
+    def __init__(self, q_text, q_answer) -> None:
         self.text = q_text
-        self.anwser = q_anwser
+        self.answer = q_answer
