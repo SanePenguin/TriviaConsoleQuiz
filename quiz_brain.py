83a3e99570614c146d8170012ba6c0857eda24a3
René Delion <rene.leon.delion@gmail.com>
2023-11-03 17:12:22 +0100

    Add next_question Method to QuizBrain

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,9 @@
 class QuizBrain:
-    def __init__(self, question_list) -> None:
+    def __init__(self, q_list) -> None:
         self.question_number = 0
-        self.question_list = question_list
+        self.question_list = q_list
+    
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True or False)?: ")
