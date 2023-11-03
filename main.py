499737b51bd4c78a50eccf70de4f5b7642350adb
René Delion <rene.leon.delion@gmail.com>
2023-11-03 17:18:43 +0100

    Add main game loop

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 for q in question_data:
     question_bank.append(Question(q["text"], q["answer"]))
+
+quiz = QuizBrain(question_bank)
+
+#start game
+while quiz.still_has_questions():
+    quiz.next_question()
